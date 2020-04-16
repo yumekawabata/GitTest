3caff88e7fd451c35449db73924d0a67c56b8979
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-04-16 10:48:58 +0900

    first commit

---
+++ GitTect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTect
+//
+//  Created by ASW-研修３ on 2020/04/16.
+//  Copyright © 2020 ASW-研修３. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-GitTect/Pods-GitTect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitTect : NSObject
+@end
+@implementation PodsDummy_Pods_GitTect
+@end
